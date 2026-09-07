aaff4b46fc308078320bd6980d852db30bc698c9
asberg95 <a.s.van.den.berg@fsw.leidenuniv.nl>
2026-09-07 09:16:08 +0000

    Deploying to gh-pages from @ mirremirre/Social_Inequality_Lab@f86b0cf45b0bfee4850e1cdaa5cf45d21f09ce87 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -80,7 +80,7 @@ ninja.data = [{
               window.location.href = "/projects/5_project/";
             },},{id: "projects-income-volatility-psychological-states-and-saving-decisions",
           title: 'Income Volatility, Psychological States, and Saving Decisions',
-          description: "Investigate how within-year income volatility drives financial stress, risk aversion, and suboptimal financial decision-making",
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/6_project/";
             },},{id: "projects-retirement-planning-among-financially-vulnerable-workers",
